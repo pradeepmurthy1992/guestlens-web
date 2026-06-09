e63fa4f8ca7f82b91c65a441abbadc6de25690f9
pradeepmurthy1992 <pradeepmurthy8@gmail.com>
2026-06-09 20:28:47 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,12 @@ import Navbar from "../components/layout/Navbar";
 import Footer from "../components/layout/Footer";
 
 import Hero from "../features/landing/Hero";
+import HowItWorks from "../features/landing/HowItWorks";
+import Features from "../features/landing/Features";
+import Timeline from "../features/landing/Timeline";
+import Pricing from "../features/landing/Pricing";
+import Faq from "../features/landing/Faq";
+import FinalCTA from "../features/landing/FinalCTA";
 
 export default function Home() {
   return (
@@ -9,6 +15,18 @@ export default function Home() {
       <Navbar />
 
       <Hero />
+
+      <HowItWorks />
+
+      <Features />
+
+      <Timeline />
+
+      <Pricing />
+
+      <Faq />
+
+      <FinalCTA />
 
       <Footer />
     </>
